ee3e01875cfb57381401e0c450ce2f3f735b9582
Liz303 <lhenderson303@gmail.com>
2016-01-18 15:46:00 -0700

    moved console.html into public and changed it to be accessible at root

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import schema from './graph/schema';
+import path from 'path';
 
 import {graphql}      from 'graphql';
 import bodyParser     from 'body-parser';
 import APIAdapter     from './adapters/api_adapter';
 
 let app  = express();
-app.use(express.static(__dirname));
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/graphql', graphqlHTTP(() => ({
         schema: schema
     })));
