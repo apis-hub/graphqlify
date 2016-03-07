8b017887f13e32d33fd044ad6d63b532cea1539f
Jason Waldrip <jason@waldrip.net>
2016-03-07 15:21:45 -0700

    add number of assets to section

--- graph/types/Section.js
+++ graph/types/Section.js
@@ -5,6 +5,7 @@ const { type, connectionType, edgeType } = buildResourceType('Section', () => ({
   attributes: {
     name: new types.GraphQLNonNull(types.GraphQLString),
     default_asset_type: new types.GraphQLNonNull(types.GraphQLString),
+    number_of_assets: new types.GraphQLNonNull(types.GraphQLInt),
     position: new types.GraphQLNonNull(types.GraphQLInt),
     created_at: new types.GraphQLNonNull(types.GraphQLString),
     updated_at: new types.GraphQLNonNull(types.GraphQLString)
